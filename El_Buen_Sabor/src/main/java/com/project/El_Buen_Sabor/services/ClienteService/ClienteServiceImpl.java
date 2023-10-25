134b9dc03f8d4d597818d3ad315b5d0cb5bb7cf3
Franco927 <ivani.franco@yahoo.com>
2023-10-25 00:16:42 -0300

    Changes by valen

--- El_Buen_Sabor/src/main/java/com/project/El_Buen_Sabor/services/ClienteService/ClienteServiceImpl.java
+++ El_Buen_Sabor/src/main/java/com/project/El_Buen_Sabor/services/ClienteService/ClienteServiceImpl.java
@@ -1,11 +1,9 @@
 package com.project.El_Buen_Sabor.services.ClienteService;
 
-import com.project.El_Buen_Sabor.entities.ArticuloInsumo;
 import com.project.El_Buen_Sabor.entities.Cliente;
 import com.project.El_Buen_Sabor.repositories.BaseRepository;
 import com.project.El_Buen_Sabor.repositories.ClienteRepository;
 import com.project.El_Buen_Sabor.services.BaseService.BaseServiceImpl;
-import com.project.El_Buen_Sabor.services.ClienteService.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
